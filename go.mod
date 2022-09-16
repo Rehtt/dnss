607db5e2b116301c5b317e7bb12fa4b6dffb6987
Rehtt <dsreshiram@gmail.com>
2022-09-16 11:42:41 +0800

    :tada:

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Rehtt/dnss
+
+go 1.19
+
+require (
+	github.com/Rehtt/Kit v0.0.0-20220915090355-120c3bc68ec4 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"github.com/Rehtt/Kit/link"
+	"github.com/fsnotify/fsnotify"
+	"golang.org/x/net/dns/dnsmessage"
+	"gopkg.in/ini.v1"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var (
+	msgPool = sync.Pool{New: func() any {
+		return new(dnsmessage.Message)
+	}}
+	cache = make(map[string][4]byte)
+	mu    sync.RWMutex
+)
+
+func init() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalln("监听文件失败：", err)
+	}
+	err = watcher.Add("hosts.ini")
+	if err != nil {
+		log.Fatalln("监听文件失败：", err)
+	}
+	go func() {
+		for {
+			select {
+			case _, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				parsefile()
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+	parsefile()
+}
+
+func main() {
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 53})
+	if err != nil {
+		log.Fatalln("监听失败：", err)
+	}
+	defer conn.Close()
+	log.Println("run")
+	var (
+		buf   = make([]byte, 512)
+		addrs = link.NewDLink()
+	)
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Println("err1:", err)
+			continue
+		}
+		msg := msgPool.Get().(*dnsmessage.Message)
+
+		if err = msg.Unpack(buf[:n]); err != nil {
+			log.Println("dns unpack err:", err)
+			continue
+		}
+
+		go func(addr *net.UDPAddr, conn *net.UDPConn, msg *dnsmessage.Message) {
+			defer msgPool.Put(msg)
+
+			data, _ := msg.Pack()
+			// 返回上游查询的结果
+			if msg.Header.Response {
+				conn.WriteTo(data, addrs.Pull().(*net.UDPAddr))
+				return
+			}
+
+			for _, question := range msg.Questions {
+				switch question.Type {
+				case dnsmessage.TypeA:
+					mu.RLock()
+					ip, ok := cache[question.Name.String()]
+					mu.RUnlock()
+					if ok {
+						msg.Answers = append(msg.Answers, dnsmessage.Resource{
+							Header: dnsmessage.ResourceHeader{
+								Name:  question.Name,
+								Class: question.Class,
+								TTL:   600,
+							},
+							Body: &dnsmessage.AResource{A: ip},
+						})
+						msg.Response = true
+						data, _ := msg.Pack()
+						conn.WriteTo(data, addr)
+						continue
+					}
+				}
+				// 查询上游服务器
+				conn.WriteTo(data, &net.UDPAddr{IP: net.IP{8, 8, 8, 8}, Port: 53})
+				addrs.Push(addr)
+			}
+
+		}(addr, conn, msg)
+	}
+}
+
+func parsefile() {
+	mu.Lock()
+	defer mu.Unlock()
+	data, err := ini.Load("hosts.ini")
+	if err != nil {
+		return
+	}
+	cache = make(map[string][4]byte, len(data.Section("hosts").Keys()))
+	for _, s := range data.Section("hosts").Keys() {
+		ip := strings.Split(s.String(), ".")
+		if len(ip) != 4 {
+			continue
+		}
+		b := [4]byte{}
+		for i, v := range ip {
+			num, err := strconv.Atoi(v)
+			if err != nil {
+				continue
+			}
+			if num < 0 || num > 255 {
+				continue
+			}
+			b[i] = byte(num)
+		}
+		cache[s.Name()+"."] = b
+	}
+}
